cff4879e126f18c6d84ca13556dd867f41cab853
xdarov <xdarovv@gmail.com>
2022-11-18 13:06:40 +0300

    bug fixing

--- aplicant_pars.py
+++ aplicant_pars.py
@@ -42,33 +42,33 @@ class AplicantShop:
 
     @classmethod
     def pars_run(cls):
-        try:
             while True:
-                options = Options()
-                options.set_preference(
-                    "general.useragent.override", config.user_agent)
-                driver = Firefox(options=options)
-                driver.set_page_load_timeout(7)
-                driver.get('https://applicant.21-school.ru/shop')
-                sleep(5)
-                cls.authorization(driver)
-                for i in range(10):
+                try:
+                    options = Options()
+                    options.set_preference(
+                        "general.useragent.override", config.user_agent)
+                    driver = Firefox(options=options)
+                    driver.set_page_load_timeout(7)
+                    driver.get('https://applicant.21-school.ru/shop')
+                    sleep(5)
+                    cls.authorization(driver)
+                    for i in range(10):
+                        try:
+                            sleep(10)
+                            driver.refresh()
+                            cls.check_shop(driver)
+                        except (NoSuchElementException, TimeoutException) as e:
+                            print(f'-->ERROR<-- {e}')
+                    raise StopIteration("RESTART")
+                except Exception as e:
+                    print(f'-->ERROR<-- {e}')
+                    os.system(
+                        f"echo '{datetime.now()} -->ERROR<-- {e}' >> LOG.txt")
+                finally:
                     try:
-                        sleep(10)
-                        driver.refresh()
-                        cls.check_shop(driver)
-                    except (NoSuchElementException, TimeoutException) as e:
-                        print(f'-->ERROR<-- {e}')
-                raise StopIteration("RESTART")
-        except Exception as e:
-            print(f'-->ERROR<-- {e}')
-            os.system(
-                f"echo '{datetime.now()} -->ERROR<-- {e}' >> LOG.txt")
-        finally:
-            try:
-                driver.close()
-            except NoSuchWindowException:
-                pass
+                        driver.close()
+                    except NoSuchWindowException:
+                        pass
 
 
 if __name__ == '__main__':
